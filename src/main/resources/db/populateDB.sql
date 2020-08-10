705e2b84359fa5e1ad0cb97018f00c708a81ed15
drem1lin <drem1lin@gmail.com>
2020-08-10 09:29:42 +0300

    Add vote table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -20,3 +20,8 @@ INSERT INTO dishes (restaurant_id, name, menuDate, price)
 VALUES (100002, 'Hamburger', '2020-01-30 10:00:00', 700),
        (100002,'French fries', '2020-01-30 10:00:00', 150),
        (100003,'Chicken soup', '2020-01-31 10:00:00', 250);
+
+INSERT INTO votes (voteDate, user_id, restaurant_id)
+VALUES ('2020-01-30 10:00:00', 100000, 100002),
+       ('2020-01-30 10:00:00', 100001, 100003),
+       ('2020-01-31 10:00:00', 100000, 100003)
